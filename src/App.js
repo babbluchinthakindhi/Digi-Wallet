18636e610b89242c30a131f291df90a087398aaa
babbluchinthakindhi <162863641+babbluchinthakindhi@users.noreply.github.com>
2025-01-04 19:29:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./pages/components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Transactions from "./pages/Transactions";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+
 
 function App() {
   return (
@@ -15,6 +18,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/transactions" element={<Transactions />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
